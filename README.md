6b51c389f668ee4e60c26858d39fda231bf6e0b9
Guillaume Beaudoin <gbeaudoi@f6r12s4.clusters.42paris.fr>
2025-05-21 13:27:02 +0200

    created database and fixed the docker-compose and docker dev side

--- README.md
+++ README.md
@@ -3,3 +3,13 @@ Commands:
 - 'make dev_restart' ==> launch the dev application with Vite as dataframe on localhost:5173 for the time being
 - 'make clean' ==> clean all unnecessary files
 - 'make dev clean' ==> clean all unnecessary files on the dev side
+
+To remove the persistant database of login/users:
+- docker volume rm
+
+To check the database:
+- docker exec -it backend sh
+- sqlite3 data/db
+- sqlite> .tables
+- sqlite> SELECT * FROM users;
+- sqlite> .exit
--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import Fastify from 'fastify';
 import cors from '@fastify/cors';
+import sqlite3 from 'sqlite3'
 
 const server = Fastify();
 
@@ -7,26 +8,41 @@ server.register(cors, {
   origin: 'http://localhost:5173'
 });
 
-// // Log all incoming requests
-// server.addHook('onRequest', async (request, reply) => {
-//   console.log(`[${request.method}] ${request.url}`);
-// });
 
-// Actual route
-server.post('/api/signup', async (request, reply) => {
-  console.log("Signup request received:", request.body);
-  return { message: "Signup successful" };
+//  SECTION TO SIGN - UP USERS INTO THE DATABASE
+//create and run the database
+const db = new sqlite3.Database('./data/db');
+db.prepare(`
+  CREATE TABLE IF NOT EXISTS users (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL
+  )
+`).run();
+//register the actual route
+server.post('/api/signup', async (req, reply) => {
+  const { username, email, password } = req.body;
+  if (!username || !email || !password) 
+  {
+    return reply.code(400).send({error: 'Missing fields'});
+  }
+  try
+  {
+    const stmt = db.prepare(
+      'INSERT INTO users (username, email, password) VALUES (?, ?, ?)'
+    );
+    stmt.run(username, email, password);
+    console.log(`User stored: ${username}, ${email}`);
+  return { message: 'Signup successful' };
+  }
+  catch
+  {
+    console.log('DB insert error:', err);
+    return reply.code(500).send({error: 'Database error'});
+  }
 });
-
-// server.route({
-//   method: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-//   url: '*',
-//   handler: async (request, reply) => {
-//     console.log(`Unhandled route: ${request.method} ${request.url}`);
-//     reply.code(404).send({ error: "Not found" });
-//   }
-// });
-
+// END OF SIGN UP SECTION
 
 server.listen({ port: 3000, host: '0.0.0.0' }, (err) => {
   if (err) {
